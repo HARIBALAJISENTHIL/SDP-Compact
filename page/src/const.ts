70a3e82694c8297404d20ffb4636e2ddff618ff3
HARIBALAJI <114165617+HARIBALAJISENTHIL@users.noreply.github.com>
2025-05-16 20:39:32 +0530

    Add files via upload

---
+++ page/src/const.ts
@@ -0,0 +1,2 @@
+export const githubLink = "https://github.com/HARIBALAJISENTHIL/secure-file-sharing";
+export const npmLink = "https://www.npmjs.com/package/sdp-compact";
---
+++ page/src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import "../app.css";
+  import Navbar from "../components/Navbar.svelte";
+
+  const pageDescription = "";
+</script>
+
+<svelte:head>
+  <title>SDP Compact Example</title>
+  <meta name="description" content={pageDescription} />
+  <meta name="theme-color" content="#fff" />
+  <meta property="og:type" content="article" />
+  <meta property="og:title" content="SDP Compact Example" />
+  <meta property="og:description" content={pageDescription} />
+</svelte:head>
+
+<div class="flex flex-col h-screen">
+  <Navbar />
+  <div class="flex-grow container mx-auto justify-items-center">
+    <slot />
+  </div>
+</div>
